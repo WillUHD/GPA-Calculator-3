3e566e36472dff7d718bca12f3368583289fa79f
Will C <134638202+WillUHD@users.noreply.github.com>
2026-03-30 08:08:43 +0800

    Delete GPA Calculator directory

--- GPA Calculator/Model/UserCourseInput.swift
+++
@@ -1,5 +0,0 @@
-// `UserCourseInput` represents user's input on a particular course
-struct UserCourseInput {
-    var levelIndex: Int  // Index of the selected level for a course
-    var scoreIndex: Int  // Index of the selected score for a course
-}
